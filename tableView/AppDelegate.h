2abc18aca2d6336b927013af32dff77ee6b62262
youli <youliwang@youlidembp.lan>
2015-11-25 10:25:17 +0800

    Initial Commit

---
+++ tableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableView
+//
+//  Created by youli  on 15/11/25.
+//  Copyright © 2015年 youli . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tableView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tableView
+//
+//  Created by youli  on 15/11/25.
+//  Copyright © 2015年 youli . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
